f6013de592bbcc3b112908fee43c086417a042b6
hilmiwismadi <dzakiwismadi@gmail.com>
2025-11-21 07:08:08 +0700

    feat: npm build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: process.cwd(),
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
